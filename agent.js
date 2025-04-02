6b3851f23f572ab4a1cfa8bae2778f4e10c272a7
Vinit Shirbhate <vinit.shirbhate23@vit.edu>
2025-04-02 20:20:31 +0530

    Memory saving

--- agent.js
+++ agent.js
@@ -1,23 +1,63 @@
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatGroq } from "@langchain/groq";
+import { tool } from "@langchain/core/tools";
+import { z } from "zod";
+import { MemorySaver } from "@langchain/langgraph";
+const weatherTool = tool(
+  async ({ query }) => {
+    console.log("query", query);
+
+    return { message: `The weather in Tokyo is sunny.` };
+  },
+  {
+    name: "weather",
+    description: "get the weather in a given location",
+    schema: z.object({
+      query: z.string().describe("the query to use in search"),
+    }),
+  }
+);
 
 const model = new ChatGroq({
   model: "llama-3.3-70b-versatile",
   temperature: 0,
 });
 
+const checkpointSaver = new MemorySaver();
+
 const agent = createReactAgent({
   llm: model,
-  tools: [],
+  tools: [weatherTool],
+  checkpointSaver,
 });
 
-const result = await agent.invoke({
-  messages: [
-    {
-      role: "user",
-      content: "Hello,how can we help me?",
-    },
-  ],
-});
+const result = await agent.invoke(
+  {
+    messages: [
+      {
+        role: "user",
+        content: "what's the weather in tokyo?",
+      },
+    ],
+  },
+  {
+    configurable: { thread_id: 1 },
+  }
+);
+
+const followUp = await agent.invoke(
+  {
+    messages: [
+      {
+        role: "user",
+        content: "what city is it for?",
+      },
+    ],
+  },
+  {
+    configurable: { thread_id: 1 },
+  }
+);
 
 console.log(result.messages.at(-1)?.content);
+console.log("Follow up", followUp.messages.at(-1)?.content);
